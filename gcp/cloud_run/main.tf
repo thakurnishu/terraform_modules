54da730511a2713c8d041fcf06a79703addb44d6
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-08-05 17:38:00 +0530

    update

--- gcp/cloud_run/main.tf
+++ gcp/cloud_run/main.tf
@@ -11,9 +11,11 @@ resource "google_cloud_run_v2_service" "default" {
   template {
     containers {
       image = var.image
+
       ports {
         container_port = var.port
       }
+
       resources {
         limits = {
           cpu    = var.cpu
@@ -23,31 +25,24 @@ resource "google_cloud_run_v2_service" "default" {
     }
   }
 }
-
-data "google_iam_policy" "cloud_run_policy" {
-  dynamic "binding" {
-    for_each = local.has_service_account ? [1] : []
-    content {
-      role    = "roles/run.invoker"
-      members = ["serviceAccount:${var.service_account}"]
-    }
-  }
-
-  dynamic "binding" {
-    for_each = !local.has_service_account ? [1] : []
-    content {
-      role    = "roles/run.invoker"
-      members = var.allow_unauthenticated ? ["allUsers"] : ["allAuthenticatedUsers"]
-    }
-  }
+resource "google_cloud_run_service_iam_member" "unauthenticated" {
+  count     = local.has_service_account ? 0 : 1
+  location  = google_cloud_run_v2_service.default.location
+  project   = google_cloud_run_v2_service.default.project
+  service   = google_cloud_run_v2_service.default.name
+  role      = "roles/run.invoker"
+  member    = var.allow_unauthenticated ? "allUsers" : "allAuthenticatedUsers"
 }
 
-resource "google_cloud_run_service_iam_policy" "auth" {
-  location    = google_cloud_run_v2_service.default.location
-  project     = google_cloud_run_v2_service.default.project
-  service     = google_cloud_run_v2_service.default.name
-  policy_data = data.google_iam_policy.cloud_run_policy.policy_data
+resource "google_cloud_run_service_iam_member" "invoker" {
+  count     = local.has_service_account ? 1 : 0
+  location  = google_cloud_run_v2_service.default.location
+  project   = google_cloud_run_v2_service.default.project
+  service   = google_cloud_run_v2_service.default.name
+  role      = "roles/run.invoker"
+  member    = "serviceAccount:${var.service_account}"
 }
+
 
 output "url" {
   value = google_cloud_run_v2_service.default.uri
